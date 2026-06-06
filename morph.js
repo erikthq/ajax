4353e2455d08418cb534091699d2a12bb81b4905
Erik Thalén <erik.thalen@hyperisland.se>
2026-06-06 12:44:56 +0200

    deploy all together

--- morph.js
+++
@@ -1,2 +0,0 @@
-import{Idiomorph as n}from"idiomorph";function o(e,t,r){return n.morph(e,t,{morphStyle:r==="innerHTML"?"innerHTML":"outerHTML"}),e}var m={swap:o};export{m as morphPlugin};
-//# sourceMappingURL=morph.js.map
